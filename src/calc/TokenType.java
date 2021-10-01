9a367be26ced4f63c4ce8df7c4510bda4e3a6128
Loading BG <51295058+LoadingBG@users.noreply.github.com>
2021-10-01 12:35:22 +0300

    Initial commit

---
+++ src/calc/TokenType.java
@@ -0,0 +1,35 @@
+package calc;
+
+enum TokenType {
+    NUM(-1),
+    ADD(1),
+    SUB(1),
+    MUL(2),
+    DIV(2),
+    NEG(10),
+    OPEN_PAREN(0),
+    CLOSE_PAREN(6);
+
+    private int priority;
+
+    TokenType(int priority) {
+        this.priority = priority;
+    }
+
+    int priority() {
+        return priority;
+    }
+
+    static TokenType fromString(String token) {
+        return switch (token) {
+            case "+" -> ADD;
+            case "-" -> SUB;
+            case "*" -> MUL;
+            case "/" -> DIV;
+            case "_" -> NEG;
+            case "(" -> OPEN_PAREN;
+            case ")" -> CLOSE_PAREN;
+            default  -> NUM;
+        };
+    }
+}
